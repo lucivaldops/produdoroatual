744b10aaed75997a8a89a3b22d05f9c0104f3d3e
lucivaldops <105608988+lucivaldops@users.noreply.github.com>
2024-11-22 09:44:43 -0300

    inicio de aplicação

---
+++ podudoro/src/main/java/dev/wakandaacademy/podudoro/PodudoroApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.podudoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodudoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodudoroApplication.class, args);
+	}
+
+}
---
+++ podudoro/src/test/java/dev/wakandaacademy/podudoro/PodudoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.podudoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodudoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
